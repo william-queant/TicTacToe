4ad72b0d1ccf74b4a5637f5fc031131c03af8e8c
william-queant <william.queant@gmail.com>
2023-10-25 12:47:14 +1300

    Split the script in functions

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict";
 
-// Board is a square multiple array. Entries are either "X", "O", or null
-function getWinner(board) {
-  let inProgress = false;
-  let winner = [];
-
-  // Winning scenarios
+function winningScenarios(board) {
   const horizontal = [...board].map((row, r) =>
     row.map((_, c) => c + r * row.length)
   );
@@ -18,31 +13,10 @@ function getWinner(board) {
     line.map((_, i) => board.length - 1 + i * (board.length - 1)),
   ];
 
-  const scenarios = [...horizontal, ...vertical, ...diagonal];
+  return [...horizontal, ...vertical, ...diagonal];
+}
 
-  // flatten the board
-  const flatBoard = board.reduce((acc, val) => acc.concat(val), []);
-
-  // Check for winning scenarios
-  scenarios.forEach((scenario) => {
-    // Build a list of the entries in the board that match the scenario
-    const entries = scenario.map((index) => flatBoard[index]);
-
-    // Check if all entries are the same by removing duplicates
-    const uniqueEntries = [...new Set(entries)];
-
-    // If there is a null entry, the game is still in progress
-    if (uniqueEntries.includes(null)) {
-      inProgress = true; // still in progress
-      return;
-    }
-
-    // If there is only one entry and it is not null, we have a winner
-    if (uniqueEntries.length === 1) {
-      winner.push(uniqueEntries[0]);
-      return;
-    }
-  });
+function returnResult(inProgress, winner) {
   // In progress if there is no winner and there are still null entries
   if (inProgress && !winner.length) {
     return "Game in progress";
@@ -64,4 +38,38 @@ function getWinner(board) {
     .sort((a, b) => b.count - a.count)[0];
 
   return `Winner is ${greatWinner.player} (x${greatWinner.count} wins)`;
+}
+
+// Board is a square multiple array. Entries are either "X", "O", or null
+function getWinner(board) {
+  // flatten the board
+  const flatBoard = board.reduce((acc, val) => acc.concat(val), []);
+
+  // Board analysis results
+  let inProgress = false;
+  const winner = [];
+
+  // Check for winning scenarios
+  winningScenarios(board).forEach((scenario) => {
+    // Build a list of the entries in the board that match the scenario
+    const entries = scenario.map((index) => flatBoard[index]);
+
+    // Check if all entries are the same by removing duplicates
+    const uniqueEntries = [...new Set(entries)];
+
+    // If there is a null entry, the game is still in progress
+    if (uniqueEntries.includes(null)) {
+      inProgress = true; // still in progress
+      return;
+    }
+
+    // If there is only one entry and it is not null, we have a winner
+    if (uniqueEntries.length === 1) {
+      winner.push(uniqueEntries[0]);
+      return;
+    }
+  });
+
+  // Return the result
+  returnResult(inProgress, winner);
 }
